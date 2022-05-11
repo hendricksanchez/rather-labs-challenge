2dc8b9d78bc6b90f38a3c4afbdd8a14ff2644665
Hendrick <hendrick.sanchez@gmail.com>
2022-05-10 22:36:54 -0300

    Some packages were added * Settings for Linter, Next, PostCss and Tailwind were set * Some global css * WIP Header component

---
+++ components/Header.jsx
@@ -0,0 +1,35 @@
+// import { Layout } from 'antd';
+
+// const { Header: HeaderAnt } = Layout
+
+const Header = () => {
+  return (
+    // <HeaderAnt style={{ display: 'flex', flexDirection: 'row',  }}>
+    //   Aquiiii
+    // </HeaderAnt>
+    <header className="flex flex-row items-center justify-between drop-shadow-md py-2 px-5 bg-white">
+      <div className="flex flex-row justify-center items-center cursor-pointer">
+        <img
+          className="w-6 h-6 object-contain cursor-pointer"
+          // src={ethLogo}
+          alt="Etherium Logo"
+        />
+        <span>Dalto</span>
+      </div>
+      <nav className="flex flex-row justify-center items-center list-none">
+        <li className="cursor-pointer mr-3 hover:text-blue-500">Pricing</li>
+        <li className="cursor-pointer mr-3 hover:text-blue-500">Docs</li>
+        <li className="cursor-pointer mr-3">
+          <button
+            // onClick={() => setGlobalState('modal', 'scale-100')}
+            className="text-white bg-blue-500 py-2 px-5 rounded-xl drop-shadow-xl border border-transparent hover:bg-transparent hover:text-blue-500 hover:border hover:border-blue-500 focus:outline-none focus:ring"
+          >
+            Send
+          </button>
+        </li>
+      </nav>
+    </header>
+  );
+}
+ 
+export default Header;
