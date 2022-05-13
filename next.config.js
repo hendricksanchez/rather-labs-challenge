1c39d794dfc62059de10f5d3be1c78de970937d1
Hendrick <hendrick.sanchez@gmail.com>
2022-05-13 15:57:09 -0300

    web3, dotenv and cross-env packages were added * Environment variables * Another changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
+const stage = process.env.environment || 'production';
+console.log("stage", stage);
+const { parsed: envVariables } = require('dotenv').config({
+  path: `./src/environments/${stage}.env`,
+});
+console.log("envVariables", envVariables);
+
 module.exports = {
   reactStrictMode: true,
+  env: {
+    ...envVariables,
+    ENVIRONMENT: process.env.environment,
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,47 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import { contractAbi } from '../abi/abi';
 
 const Index = () => {
+  const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(true);
 
-  const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(false);
+  // const checkTokenBalance = async (_contract) => {
+    // const accounts = await window.ethereum.enable();
+    // console.log("accounts", accounts);
+    // const balance = await _contract.methods.cooldownSeconds().call();
+    // console.log("balance", balance);
+  // }
 
+  const connectToBlockchain = async () => {
+    const web3 = new Web3(Web3.givenProvider);
+    const contract = new web3.eth.Contract(contractAbi, process.env.CONTRACT_ADDRESS);
+    console.log("contract", contract);
+
+    const accounts = await window.ethereum.enable();
+    console.log("accounts", accounts);
+
+    // const totalSupply = await contract.methods.cooldownSeconds().call();
+    // console.log("totalSupply", totalSupply);
+
+    // checkTokenBalance(contract);
+    const id = await web3.eth.net.getId();
+    console.log("id", id);
+    const addresses = await web3.eth.getAccounts();
+    console.log("addresses", addresses);
+  }
+  
   useEffect(() => {
     const { ethereum } = window;
-    if (ethereum)
-      setIsMetamaskInstalled(true);
+    if (!ethereum) {
+      setIsMetamaskInstalled(false);
+    }
+    else {
+      connectToBlockchain();
+    }
+
+
+
   }, []);
 
   return (
@@ -37,20 +70,18 @@ const Index = () => {
           </div>
 
           <div className="flex flex-col items-center justify-center h-80">
-            {isMetamaskInstalled
-              ? (
+            {/* {isMetamaskInstalled && (
               <div className="flex flex-col items-center justify-center ">
-                <p className="text-3xl my-4">First, let's to connect to the wallet</p>
+                <p className="text-3xl my-4">First, let`s to connect to the wallet</p>
                 <button type="button" className="btn btn-blue">Connect Wallet</button>
               </div>
-              )
-              : (
-                <div className="flex items-center bg-orange-600 text-white text-sm font-bold px-4 py-3" role="alert">
-                  <svg className="fill-current h-6 w-6 text-white mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
-                  <p>Metamask Wallet is not installed, please install it.</p>
-                </div>
-              )
-            }
+            )} */}
+            {!isMetamaskInstalled && (
+              <div className="flex items-center bg-orange-600 text-white text-sm font-bold px-4 py-3" role="alert">
+                <svg className="fill-current h-6 w-6 text-white mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg>
+                <p>Metamask Wallet is not installed, please install it.</p>
+              </div>
+            )}
           </div>
           
         </div>
